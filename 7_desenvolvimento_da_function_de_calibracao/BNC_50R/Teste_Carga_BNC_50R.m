7f783a05ef30ee30868d82a89e3a87baa9e59dad
ViLPado <157078489+ViLPado@users.noreply.github.com>
2024-12-10 23:47:11 -0300

    Add files via upload

---
+++ 7_desenvolvimento_da_function_de_calibracao/BNC_50R/Teste_Carga_BNC_50R.m
@@ -0,0 +1,129 @@
+%Teste BNC 50R
+
+tic;
+clc
+clear
+
+
+% Listar os arquivos da pasta de interesse
+
+%Variando a janela
+Arquivo = "D:\dados_usuario\Documents\GEDRE\TCC_Git\Código Matlab\7_desenvolvimento_da_function_de_calibracao\BNC_50R\50R\V3";
+APENAS_ARQUIVOS_CSV = "\*.csv";
+
+y = dir(Arquivo + APENAS_ARQUIVOS_CSV);
+
+Pot = zeros(1,length(y));
+S = zeros(1,length(y));
+FP = zeros(1,length(y));
+teta = zeros(1,length(y));
+Z = zeros(1,length(y));
+Zret = zeros(1,length(y));
+freq_fft = zeros(1,length(y));
+freq_t = zeros(1,length(y));
+Dtt = zeros(1,length(y));
+
+% Exibir o caminho de cada arquivo .txt na pasta
+for i = 1:length(y)
+    caminho = [y(i).folder '\' y(i).name];
+    disp(y(i).name);
+    [Pot(i),S(i),FP(i),teta(i),Z(i),Zret(i),freq_fft(i),freq_t(i), Dtt(i)] = dadoCSVslim(caminho,1);
+end
+
+
+Amostra = 1:length(y);
+
+
+tab = table(Amostra',Pot', S', FP', teta', Z', Zret', freq_fft', freq_t', Dtt', 'VariableNames', {'Amostras','Pot', 'S', 'FP', 'teta', 'Z', 'Zret', 'freq pela fft', 'freq pelo delta tempo', 'freq pelo todo tempo'});
+
+tab_mean = table({'Média'}, mean(Pot), mean(S), mean(FP), mean(teta), mean(Z), mean(Zret), mean(freq_fft), mean(freq_t), mean(Dtt), 'VariableNames', {'Amostras','Pot', 'S', 'FP', 'teta', 'Z', 'Zret', 'freq pela fft', 'freq pelo delta tempo', 'freq pelo todo tempo'});
+
+disp(tab);
+disp(tab_mean);
+
+
+% Plotar os dados
+% Criar uma figura
+figure;
+
+% Primeiro subplot
+h(1) = subplot(2,1,1);
+semilogx(Dtt, Z, 'o-');
+xlabel('Frequência (Hz)');
+ylabel('Módulo Z (\Omega)');
+ylim([49 max(Z)]);
+title('Módulo Z vs Frequência');
+grid on;
+
+% Definir os rótulos do eixo x para mostrar os valores desejados
+xticks([10000, 100000, 1000000, 10000000]); % Define os pontos do eixo x
+xticklabels({'10kHz', '100kHz', '1MHz', '10MHz'}); % Define os rótulos correspondentes
+
+% Ajustar a aparência dos rótulos do eixo x
+xtickangle(45); % Rotaciona os rótulos para melhor legibilidade, se necessário
+
+% Segundo subplot
+h(2) = subplot(2,1,2);
+semilogx(Dtt, teta, 'o-');
+xlabel('Frequência (Hz)');
+ylabel('Ângulo teta (°)');
+ylim([-10 max(teta)]);
+title('Ângulo teta vs Frequência');
+grid on;
+
+% Definir os rótulos do eixo x para mostrar os valores desejados
+xticks([10000, 100000, 1000000, 10000000]); % Define os pontos do eixo x
+xticklabels({'10kHz', '100kHz', '1MHz', '10MHz'}); % Define os rótulos correspondentes
+
+% Ajustar a aparência dos rótulos do eixo x
+xtickangle(45); % Rotaciona os rótulos para melhor legibilidade, se necessário
+
+%linkaxes(h,'xy');
+toc;
+
+
+
+%%
+
+[Z_corrigida,teta_corrigida] = calibracao(Z,teta,Dtt);
+
+
+
+% Plotar os dados
+% Criar uma figura
+figure;
+
+% Primeiro subplot
+h(1) = subplot(2,1,1);
+semilogx(Dtt, Z_corrigida, 'o-');
+xlabel('Frequência (Hz)');
+ylabel('Módulo Z (\Omega)');
+ylim([49 max(Z)]);
+title('Módulo Z vs Frequência');
+set(gca, 'XTickLabel', []); % Remover os rótulos do eixo x
+grid on;
+
+% Definir os rótulos do eixo x para mostrar os valores desejados
+%xticks([10000, 100000, 1000000, 10000000]); % Define os pontos do eixo x
+%xticklabels({'10kHz', '100kHz', '1MHz', '10MHz'}); % Define os rótulos correspondentes
+
+% Ajustar a aparência dos rótulos do eixo x
+xtickangle(45); % Rotaciona os rótulos para melhor legibilidade, se necessário
+
+% Segundo subplot
+h(2) = subplot(2,1,2);
+semilogx(Dtt, teta_corrigida, 'o-');
+xlabel('Frequência (Hz)');
+ylim([-10 max(teta)]);
+ylabel('Ângulo teta (°)');
+title('Ângulo teta vs Frequência');
+grid on;
+
+% Definir os rótulos do eixo x para mostrar os valores desejados
+xticks([10000, 100000, 1000000, 10000000]); % Define os pontos do eixo x
+xticklabels({'10kHz', '100kHz', '1MHz', '10MHz'}); % Define os rótulos correspondentes
+
+% Ajustar a aparência dos rótulos do eixo x
+xtickangle(45); % Rotaciona os rótulos para melhor legibilidade, se necessário
+
+%linkaxes(h,'xy');
